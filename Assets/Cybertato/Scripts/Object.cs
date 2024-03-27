0013aeed16ef316b05ff8c6a159011a61787a70b
MarcusGeorgiou <1027503@student.sae.edu.au>
2024-03-28 10:34:18 +1100

    New folders and removing tags

--- Assets/Cybertato/Scripts/Object.cs
+++ Assets/Cybertato/Scripts/Object.cs
@@ -14,7 +14,7 @@ public class Object : MonoBehaviour
     }
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.gameObject.GetComponent<IPlayer>() != null)
         {
             ObjectJuggle.Invoke();
         }
