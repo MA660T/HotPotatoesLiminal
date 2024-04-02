8104fbbd3a78b92154f771dacd3eada92d8f64c7
MarcusGeorgiou <1027503@student.sae.edu.au>
2024-04-02 17:25:26 +1100

    Untested specific spawning

--- Assets/Cybertato/Scripts/Spawner.cs
+++ Assets/Cybertato/Scripts/Spawner.cs
@@ -6,24 +6,45 @@ using Random = UnityEngine.Random;
 
 public class Spawner : MonoBehaviour
 {
+    Vector3 position;
+    public ShapeBase[] shapes;
+    
+    [HideInInspector]
+    public int spawnedObjs = 0;
+    public int maxObjects = 3;
+    
+    [Header("Check box for random spawning")]
+    public bool testMode;
+    
     //TODO: Tie to GameController to handle objects to spawn
+
+    public void SpawnGivenObject(ShapeBase shapeToSpawn)
+    {
+        if (spawnedObjs < maxObjects)
+        {
+            Instantiate(shapes[(int)shapeToSpawn.myShape],
+                new Vector3(position.x + Random.Range(0.5f, 3f), position.y, position.z + Random.Range(0.5f, 3f)), 
+                Quaternion.identity);
+            spawnedObjs++;
+        }
+    }
 
     //HACK: Spawn random (up to 3)
     #region HACK SPAWN
-    public ShapeBase[] shapes;
-    [HideInInspector]
-    public int spawnedObjs = 0;
 
     private void Start()
     {
-        StartCoroutine(SpawnObject(2f));
+        if(testMode)
+            StartCoroutine(SpawnObject(2f));
+        
+        position = transform.position;
     }
 
     IEnumerator SpawnObject(float delay)
     {
-        while (spawnedObjs < 3)
+        while (spawnedObjs < maxObjects)
         {
-            Instantiate(shapes[Random.Range(0, 2)], new Vector3(transform.position.x + Random.Range(0.5f, 3f), transform.position.y, transform.position.z + Random.Range(0.5f, 3f)), Quaternion.identity);
+            Instantiate(shapes[Random.Range(0, 2)], new Vector3(position.x + Random.Range(0.5f, 3f), position.y, position.z + Random.Range(0.5f, 3f)), Quaternion.identity);
             spawnedObjs++;
             
             yield return new WaitForSeconds(delay);
