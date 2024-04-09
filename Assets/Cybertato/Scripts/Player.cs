a040b4a4c4c3fe47703faedb3908732084bc0cc8
MarcusGeorgiou <1027503@student.sae.edu.au>
2024-04-09 11:01:29 +1000

    added randomness depending on shape (untested), slight setup on UI controller

--- Assets/Cybertato/Scripts/Player.cs
+++ Assets/Cybertato/Scripts/Player.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Player : MonoBehaviour, IPlayer
 {
@@ -17,17 +18,33 @@ public class Player : MonoBehaviour, IPlayer
 
     Vector2 CalculateRandomness(ShapeBase obj)
     {
-        
+        Vector2 rand = new Vector2();
         Vector2 direction = Vector2.up;
        
       //Read object and determine random range to throw object off hand
       //USE: obj.myShape to read type
       /*
-        Sphere = little to no randomness
-        Cube = Some randomness
+        Sphere = Some randomness 
+        Cube = little to no randomness 
         Bounce = High randomness
       */
+      switch (obj.myShape)
+      {
+          case Shape.Sphere:
+              rand.x = Random.Range(0f, 0.5f);
+              rand.y = Random.Range(0f, 0.5f);
+              break;
+          case Shape.Cube:
+              rand.x = Random.Range(0f, 0.2f);
+              rand.y = Random.Range(0f, 0.2f);
+              break;
+          case Shape.Bouncer:
+              rand.x = Random.Range(0f, 1f);
+              rand.y = Random.Range(0f, 1f);
+              break;
+      }
 
+      direction += rand;
       return direction;
     }
 }
--- Assets/Cybertato/Scripts/UIController.cs
+++ Assets/Cybertato/Scripts/UIController.cs
@@ -2,12 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// This script will be attached to an object on the player hand
+/// will control the information displayed only
+/// score, combo counter all read from the game manager
+/// </summary>
 public class UIController : MonoBehaviour
 {
+    private GameController gm;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        gm = FindObjectOfType<GameController>();
     }
 
     // Update is called once per frame
