cf39e3cbf2db50e70c0d1a74579ac1dac8d2fbba
MarcusGeorgiou <1027503@student.sae.edu.au>
2024-04-10 14:13:29 +1000

    added a container to stop objects getting too far away (regular drag was affecting bounce calculations, but also only thing changing fall speed), changed to delie's meshes (needed to be scaled)

---
+++ Assets/Cybertato/Scripts/Container.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Container : MonoBehaviour
+{
+    private void OnTriggerExit(Collider other)
+    {
+        ShapeBase shape = other.GetComponent<ShapeBase>();
+
+        if (shape != null)
+        {
+            shape.gameObject.GetComponent<Rigidbody>().velocity *= -1f;
+        }
+    }
+}
