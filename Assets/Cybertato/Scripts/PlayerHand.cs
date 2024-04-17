2679968065b864578fcc407c870c8724c3bd0d12
Michael <m.a.isands2002@gmail.com>
2024-04-17 14:04:16 +1000

    added haptics and hand plate renderers

--- Assets/Cybertato/Scripts/PlayerHand.cs
+++ Assets/Cybertato/Scripts/PlayerHand.cs
@@ -10,6 +10,7 @@ public class PlayerHand : MonoBehaviour
     [Tooltip("Add the collider of THIS INSTANCE of the Player Hand")]
     public Collider myCollider;
     public event Action<Collider, Collision> hitEvent;
+    
 
     AudioSource SFX;
 
@@ -20,7 +21,38 @@ public class PlayerHand : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        hitEvent?.Invoke(myCollider, collision);
-        SFX.Play();
+        if (myCollider.name == "PHandCollision")
+        {
+            StartCoroutine(Haptics(1, 1, 0.3f, true, false));
+            SFX.Play();
+            StartCoroutine(Haptics(1, 1, 0.3f, false, true));
+        }
+
+
+        else if (myCollider.name == "SHandCollision")
+        {
+            StartCoroutine(Haptics(1, 1, 0.3f, true, false));
+            SFX.Play();
+            StartCoroutine(Haptics(1, 1, 0.3f, false, true));
+        }
+
+    }
+
+    IEnumerator Haptics(float frequency, float amplitude, float duration, bool rightHand, bool leftHand)
+    {
+        if (myCollider.name == "PHandCollision")
+        {
+            OVRInput.SetControllerVibration(frequency, amplitude, OVRInput.Controller.RTouch);
+            yield return new WaitForSeconds(duration);
+            OVRInput.SetControllerVibration(0, 0, OVRInput.Controller.RTouch);
+        }
+        else if(myCollider.name == "SHandCollision") 
+        {
+            OVRInput.SetControllerVibration(frequency, amplitude, OVRInput.Controller.LTouch);
+            yield return new WaitForSeconds(duration);
+            OVRInput.SetControllerVibration(0, 0, OVRInput.Controller.LTouch);
+
+        }
+
     }
 }
