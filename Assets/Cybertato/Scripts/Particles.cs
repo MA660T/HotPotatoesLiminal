134cf351a41cdf840cd0529c710d0d6c8df2af56
Michael <m.a.isands2002@gmail.com>
2024-05-01 16:56:55 +1000

    trying particles and imported a font for ui

---
+++ Assets/Cybertato/Scripts/Particles.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Particles : MonoBehaviour
+{
+    public Collider handCollider;
+    public ParticleSystem LRParticles;
+
+    // Start is called before the first frame update
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        LRParticles.Play();
+    }
+}
